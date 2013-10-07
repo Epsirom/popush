52aa8be96f0bd5b263ae3888c4a027f1d3cd4065
wenqf11 <thssvince@gmail.com>
2013-10-07 10:19:22 +0800

    flat-ui&angular-translate  added

--- static_sailon/app/js/app.js
+++ static_sailon/app/js/app.js
@@ -2,9 +2,18 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('popush', ['popush.filters', 'popush.services', 'popush.directives', 'popush.controllers']).
+var app = angular.module('popush', ['popush.filters', 'popush.services', 'popush.directives', 'popush.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/signIn.html', controller: 'SignController'});
     $routeProvider.when('/signUp', {templateUrl: 'partials/signUp.html', controller: 'SignController'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
+
+app.module('popush', ['pascalprecht.translate']);
+
+app.config(function ['$translateProvider', ($translateProvider) {
+  $translateProvider.translations({
+    HEADLINE: 'Hello there, This is my awesome app!',
+    INTRO_TEXT: 'And it has i18n support!'
+  });
+}]);
