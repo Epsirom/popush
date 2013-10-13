cd1e05590a857c76058d1fc73e1f44d251a80406
Lei Yang <yl93528@gmail.com>
2013-10-14 00:18:02 +0800

    Main Page frame

--- static_sailon/app/js/user/SignInController.js
+++ static_sailon/app/js/user/SignInController.js
@@ -2,7 +2,7 @@
 
 function SignInController($scope, userModel, socket, $location) {
 	$scope.signInFn = function() {
-
+		$location.path('/workspace');
 	};
 	$scope.switchSignUp = function() {
 		//$location.path('/signUp');
